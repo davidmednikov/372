d872e4f1de5c588fad5e73100e74fd221e4fda91
David Mednikov <davemednikov@gmail.com>
2019-11-26 01:00:25 -0800

    commenting ftserver

--- project2/ftserver.c
+++ project2/ftserver.c
@@ -11,97 +11,157 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+// define bool enums
 typedef enum { false, true } bool;
 
+// define command enums
 typedef enum { err, list, get } cmd;
 
 int open_listen_port(char* port, struct addrinfo listen_hints, struct addrinfo *listen_res) {
+    // create int for server socket file descriptor
     int socket_fd;
+
+    // clear listen port address info struct and set fields for getting server info
     memset(&listen_hints, 0, sizeof listen_hints);
     listen_hints.ai_family = AF_UNSPEC;
     listen_hints.ai_socktype = SOCK_STREAM;
     listen_hints.ai_flags = AI_PASSIVE;
+
+    // get localhost info and store to listen_res
     getaddrinfo(NULL, port, &listen_hints, &listen_res);
 
+    // open socket based on info stored in listen_res
     socket_fd = socket(listen_res->ai_family, listen_res->ai_socktype, listen_res->ai_protocol);
+
+    // bind server to socket
     bind(socket_fd, listen_res->ai_addr, listen_res->ai_addrlen);
 
+    // print update to terminal and return socket number
     printf("Server open on %s\n\n", port);
     return socket_fd;
 }
 
 int open_data_port(char* host, char* port, struct addrinfo data_hints, struct addrinfo *data_res) {
+    // create int for data socket file descriptor
     int socket_fd;
+
+    // clear data port address intfo struct and set fields for getting client info
     memset(&data_hints, 0, sizeof data_hints);
     data_hints.ai_family = AF_UNSPEC;
     data_hints.ai_socktype = SOCK_STREAM;
 
+    // get client's info and store to data_res
     getaddrinfo(host, port, &data_hints, &data_res);
 
+    // open socket pointing to client using data stored in data_res sturect
     socket_fd = socket(data_res->ai_family, data_res->ai_socktype, data_res->ai_protocol);
+
+    // connect to data socket and return socket numer
     connect(socket_fd, data_res->ai_addr, data_res->ai_addrlen);
     return socket_fd;
 }
 
 int accept_client_connection(int socket_fd, struct sockaddr_storage client_address, socklen_t address_size, char* client_host, char* client_name, char* service) {
+    // listen to socket for client connection. only accepts 1 connection at a time
     listen(socket_fd, 1);
 
+    // accept client connection and save to new socket number
     int new_fd = accept(socket_fd, (struct sockaddr *) &client_address, &address_size);
 
+    // get info about client host and store to client_address
     getpeername(new_fd, (struct sockaddr *) &client_address, &address_size);
 
+    // clear fields for client host, name, and service (port)
     memset(client_host, '\0', 100);
     memset(client_name, '\0', 10);
     memset(service, '\0', 10);
+
+    // get client hostname and port, store to client_host and service
     getnameinfo((struct sockaddr *) &client_address, address_size, client_host,
                     sizeof (char[100]), service, sizeof (char[10]), 0);
+
+    // copy first 6 chars of hostname to get host server
     strncpy(client_name, client_host, 6);
+
+    // add null terminator to end string
     client_name[5] = '\0';
+
+    // print update to terminal and return data socket to caller
     printf("Connection from %s.\n", client_name);
     return new_fd;
 }
 
 cmd get_command(int new_fd, char* buffer, char* command, char* filename, char* data_port) {
+    // define variables used in function
     int bytes_sent, spaces, string_length;
 
+    // clear strings for holding command info from client
     memset(buffer, '\0', 1000);
     memset(command, '\0', 10);
     memset(filename, '\0', 100);
     memset(data_port, '\0', 10);
 
+    // receive data from the socket
     bytes_sent = recv(new_fd, buffer, 1000, 0);
+
+    // if data received
     if (bytes_sent != 0) {
+        // get string length and check that there are at least 2 characters (minimum command length)
         string_length = strlen(buffer);
         if (string_length >= 2) {
+            // if at least 2 chars, count spaces so that we know what the client is sending
             spaces = 0;
             for (int i = 0; i < string_length; i++) {
                 if (buffer[i] == ' ') {
                     spaces++;
                 }
             }
+
+            // copy command from buffer to var and set end of string
             strncpy(command, buffer, 3);
             command[2] = '\0';
+
+            // if just one space and command is "-l", client requesting directory
             if (spaces == 1 && strcmp(command, "-l") == 0) {
+                // get first token (command)
                 char* token = strtok(buffer, " ");
+
+                // if not null, get next token and copy to data_port
                 if (token != NULL) {
                     token = strtok(NULL, " ");
                     strcpy(data_port, token);
                 }
+
+                // return list enum
                 return list;
             } else if (spaces == 2 && strcmp(command, "-g") == 0) {
+                // if 2 spaces if command is "-g", client requesting file
+
+                // get first token (command)
                 char* token = strtok(buffer, " ");
+
+                // if not null, get next token and copy to filename
                 if (token != NULL) {
                     token = strtok(NULL, " ");
                     strcpy(filename, token);
+                    // if not null, get next token and copy to data_port
                     if (token != NULL) {
                         token = strtok(NULL, " ");
                         strcpy(data_port, token);
+                    } else {
+                        // not enough tokens, invalid input - return error enum
+                        return err;
                     }
+                } else {
+                    // not enough tokens, invalid input - return error enum
+                    return err;
                 }
+                // return get enum
                 return get;
             }
         }
     }
+    // return error enum
     return err;
 }
 
